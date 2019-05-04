53f4048aaec18c754a47fa6c1bb5848beab4a0c3
Prasen Gope <prasen.gope@hotmail.com>
2019-05-04 12:26:44 -0400

    password issue resolved

--- database.js
+++ database.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const mysql = require('mysql');
 var password = require("./password");
-require("dotenv").config();
+
+console.log(password);
 
 module.exports = mysql.createConnection({
   host: "127.0.0.1",
   port: 3306,
   user: "root",
-  password: "Sylhet123",
+  password: password.database.password,
   database: "bamazon"
 })
